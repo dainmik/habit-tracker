2108e1757b5abbbffddfe4d3625e6f83a17c8b4f
Dainius Mikalajūnas <dainius@mklns.com>
2025-05-24 10:12:38 +0000

    Enfore kebab-case for files with ESLint

--- eslint.config.ts
+++ eslint.config.ts
@@ -6,6 +6,7 @@ import {
 import pluginVue from "eslint-plugin-vue";
 import pluginPlaywright from "eslint-plugin-playwright";
 import skipFormatting from "@vue/eslint-config-prettier/skip-formatting";
+import checkFile from "eslint-plugin-check-file";
 
 export default defineConfigWithVueTs(
 	{
@@ -23,4 +24,30 @@ export default defineConfigWithVueTs(
 		files: ["e2e/**/*.{test,spec}.{js,ts,jsx,tsx}"],
 	},
 	skipFormatting,
+
+	{
+		name: "check-file",
+		files: ["src/**/*.*"],
+		plugins: {
+			"check-file": checkFile,
+		},
+		rules: {
+			"check-file/filename-naming-convention": [
+				"error",
+				{
+					"**/*.{ts,tsx}": "KEBAB_CASE",
+				},
+				{
+					// Ignore the middle extensions of the filename to support filename like prettier.config.js or my.test.ts
+					ignoreMiddleExtensions: true,
+				},
+			],
+			"check-file/folder-naming-convention": [
+				"error",
+				{
+					"src/**/!(__tests__)": "KEBAB_CASE",
+				},
+			],
+		},
+	},
 );
