41d8d4fdeb756a6b42c73cc14f4360aa67a14663
Dainius Mikalajūnas <dainius@mklns.com>
2025-06-28 21:38:20 +0000

    Remove unnecessary code

--- libs/habit-tracker-data/drizzle.config.ts
+++ libs/habit-tracker-data/drizzle.config.ts
@@ -1,10 +1,6 @@
 import { defineConfig } from "drizzle-kit";
 import { env } from "./src/env.ts";
 
-if (!env.HABIT_TRACKER_DATA_DATABASE_URL) {
-	throw new Error("DB_URL environment variable is not set.");
-}
-
 // eslint-disable-next-line import-x/no-default-export
 export default defineConfig({
 	out: "./src/db/migrations",
